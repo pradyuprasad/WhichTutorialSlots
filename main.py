40f5f129be8549d8cf167d290d8661ff30423902
lavanyagarg112 <lavanyagarg@u.nus.edu>
2024-07-24 21:00:59 +0800

    lecture timings

--- main.py
+++ main.py
@@ -1,23 +1,88 @@
 import requests
 from typing import Dict, List, Any
 from pydantic import BaseModel
+from datetime import datetime, timedelta
 
 class Timings(BaseModel):
-    LectureTimings: List[Dict[Any, Any]]
+    LectureTimings: Dict[Any, Any]
     RestTimings: List[Dict[Any, Any]]
 
-class Course(BaseModel):
-    timings: Timings
+class Course(Timings):
+    available: List[Dict[Any, Any]]
 
 
-def getAllTimings(module_code:str) -> Timings:
+def getCourse(module_code:str) -> Course:
     try:
         response = requests.get(f"https://api.nusmods.com/v2/2024-2025/modules/{module_code}.json")
         ans = response.json()
     except Exception as e:
         print(e)
     
-    return Timings(RestTimings=(list(filter(lambda x: x['lessonType'] != 'Lecture', ans['semesterData'][0]['timetable']))), LectureTimings=list(filter(lambda x: x['lessonType'] == 'Lecture', ans['semesterData'][0]['timetable'])))
+    ans = ans['semesterData'][0]['timetable']
+    restTimings = list(filter(lambda x: x['lessonType'] != 'Lecture', ans))
+    lectureTimings = getLecture(list(filter(lambda x: x['lessonType'] == 'Lecture', ans)))
+    return Course(available=[], RestTimings=restTimings, LectureTimings=lectureTimings)
+
+
+def getLecture(array: List[Dict[Any, Any]]) -> Dict[Any, Any]:
+
+    finalList = {'Monday': [], 'Tuesday': [], 'Wednesday': [], 'Thursday': [], 'Friday': []}
+
+    for slot in array:
+        day = slot['day']
+        finalList[day].append([float(slot['startTime']), float(slot['endTime'])])
+
+    print(finalList)
+    return finalList
+
+
+
+def merge(intervals):
+
+    if not intervals:
+        return []
+
+    intervals.sort()
+    
+    output = []
+    start = 0
+    end = 0
+    flag = False
+    for index in range(len(intervals)):
+        i = intervals[index]
+        if not flag:
+            start = i[0]
+            end = i[1]
+            flag = True
+
+        else:
+            if i[0] <= end:
+                end = max(end,i[1])
+            else:
+                output.append([start, end])
+                start = i[0]
+                end = i[1]
+
+        if index == len(intervals) - 1:
+            output.append([start, end])
+
+
+    return output
+
+
+def getAllLectureTimings(courses: List[Course]) -> Dict[Any, List[Any]]:
+
+    finalTimings = {'Monday': [], 'Tuesday': [], 'Wednesday': [], 'Thursday': [], 'Friday': []}
+
+    for course in courses:
+        lectures = course.LectureTimings
+        for day in finalTimings:
+            finalTimings[day].extend(lectures[day])
+            finalTimings[day] = merge(finalTimings[day])
+
+    return finalTimings
+
+    
 
 
 '''
@@ -53,6 +118,9 @@ constraints
 '''
 
 
-ST_class_list = getAllTimings('ST2334')
-CS2109s_class_list = getAllTimings('CS2109S')
+CS3230_class_list = getCourse('CS3230')
+NGT2001K_class_list = getCourse('NGT2001K')
+CS2100_class_list = getCourse('CS2100')
+print(getAllLectureTimings([CS3230_class_list, CS2100_class_list, NGT2001K_class_list]))
 
+# some reason not counting ngt?
